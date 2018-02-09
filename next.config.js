03b43d042e888ccd83f488429ce2fe63ffd5d0f9
Weffe <rogelio_negrete@live.com>
2018-02-08 18:45:36 -0800

    Add babili plugin to compile the fireworks-canvas library properly

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
+const BabiliPlugin = require("babili-webpack-plugin");
+
 module.exports = {
-  exportPathMap: function () {
+  webpack(config, { dev }) {
+    // remove Uglify plugin
+    config.plugins = config.plugins.filter(plugin => {
+      return plugin.constructor.name !== "UglifyJsPlugin";
+    });
+
+    if (!dev) {
+      // add Babili plugin
+      config.plugins.push(
+        new BabiliPlugin()
+      );
+    }
+
+    return config;
+  },
+  exportPathMap() {
     return {
       '/': { page: '/' },
       '/login': { page: '/login' },
